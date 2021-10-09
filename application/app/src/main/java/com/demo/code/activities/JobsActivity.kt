012df244e00126f6c564b3d5e1005a00d2408be4
Devrath <devrath.dev595@gmail.com>
2021-10-09 17:39:10 +0530

    v

--- application/app/src/main/java/com/demo/code/activities/JobsActivity.kt
+++ application/app/src/main/java/com/demo/code/activities/JobsActivity.kt
@@ -3,8 +3,12 @@ package com.demo.code.activities
 import android.os.Bundle
 import com.demo.code.base.BaseActivity
 import com.demo.code.databinding.ActivityJobsBinding
+import kotlinx.coroutines.*
 
+@DelicateCoroutinesApi
 class JobsActivity : BaseActivity() {
+
+    override fun getToolbarTitle() = "Jobs in Coroutine"
 
     private lateinit var binding: ActivityJobsBinding
 
@@ -19,11 +23,94 @@ class JobsActivity : BaseActivity() {
     private fun setOnClickListeners() {
         binding.apply {
             launchId.setOnClickListener {
-
+                retryDemo()
             }
         }
     }
 
-    override fun getToolbarTitle() = "Jobs in Coroutine"
+    private fun simpleJobTwoDemo() {
+
+       val rootJob = GlobalScope.launch(start = CoroutineStart.DEFAULT){
+
+           val parentJob = launch {
+               delay(10)
+               println("Parent job started")
+               delay(10)
+           }
+
+           val childOneJob = launch() {
+               delay(10)
+               println("Child one job started")
+               delay(10)
+           }
+
+           val childTwoJob = launch() {
+               delay(10)
+               println("Child two job started")
+               delay(10)
+           }
+
+           if(parentJob.children.iterator().hasNext()){
+               val childCnt = parentJob.children
+               println("Parent has $childCnt children")
+           }else{
+               println("Parent has no children")
+           }
+
+           Thread.sleep(1000)
+       }
+
+        /**
+         * OUTPUT:->
+         * Parent has kotlin.sequences.SequencesKt__SequenceBuilderKt$sequence$$inlined$Sequence$1@85caa11 children
+         * Parent job started
+         * Child two job started
+         * Child one job started
+         */
+
+    }
+
+    private fun simpleJobOneDemo() {
+
+        /**
+         * This since executed by Lazy, It will start only when invoked
+         */
+        val job1 = GlobalScope.launch(start=CoroutineStart.LAZY) {
+            delay(200)
+            println("Two")
+            delay(200)
+        }
+
+        GlobalScope.launch {
+            delay(200)
+            println("One")
+            // Job is invoked here so it is executed here
+            job1.join()
+            println("Three")
+        }
+
+        /**
+         * 2021-10-09 12:08:38.108 18586-18711/com.demo.code I/System.out: One
+         * 2021-10-09 12:08:38.311 18586-18711/com.demo.code I/System.out: Two
+         * 2021-10-09 12:08:38.519 18586-18711/com.demo.code I/System.out: Three
+         */
+    }
+
+
+    private fun retryDemo(){
+        GlobalScope.launch {
+            repeat(3){
+                delay(200)
+                println("Execution happened")
+            }
+        }
+
+        /** Output:
+         * Execution happened
+         * Execution happened
+         * Execution happened
+         */
+    }
+
 
 }
