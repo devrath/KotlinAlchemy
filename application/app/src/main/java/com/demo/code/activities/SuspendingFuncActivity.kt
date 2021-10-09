42fbdfbf93a4b73156424911700c6798a3239e93
Devrath <devrath.dev595@gmail.com>
2021-10-09 21:46:56 +0530

    v

--- application/app/src/main/java/com/demo/code/activities/SuspendingFuncActivity.kt
+++ application/app/src/main/java/com/demo/code/activities/SuspendingFuncActivity.kt
@@ -1,8 +1,12 @@
 package com.demo.code.activities
 
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.demo.code.base.BaseActivity
 import com.demo.code.databinding.ActivitySuspendingFuncBinding
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
 
 class SuspendingFuncActivity  : BaseActivity() {
 
@@ -21,9 +25,43 @@ class SuspendingFuncActivity  : BaseActivity() {
     private fun setOnClickListeners() {
         binding.apply {
             btnLaunchId.setOnClickListener {
-
+                simpleDemo()
             }
         }
     }
+
+    private fun simpleDemo() {
+        lifecycleScope.launch {
+            getUser("111")
+        }
+    }
+
+    private suspend fun getUser(id: String): User = suspendCoroutine {
+        getUserFromNetworkCallback("101") { user, error ->
+            user?.run { println(this.userName) }
+            error?.run { println(this.message) }
+        }
+    }
+
+
+    private fun getUserFromNetworkCallback(
+        userId: String,
+        onUserResponse: (User?, Throwable?) -> Unit) {
+        thread {
+            try {
+                Thread.sleep(1000) // ---> Simulating the network delay
+                val user = User(userId, "Test User")
+
+                onUserResponse(user, null)
+            } catch (error: Throwable) {
+                onUserResponse(null, error)
+            }
+        }
+    }
+
+
+    data class User(val userId:String,val userName:String)
+
+
 
 }
