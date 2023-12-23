c0508d411874febcaf96e072afd8f9435ca9468a
Devrath <devrath.dev595@gmail.com>
2023-12-23 22:33:38 +0530

    added scroll

--- application/KotlinConcepts/app/src/main/java/com/istudio/app/modules/module_demos/basic_fundamentals/ui/KotlinBasicsDemo.kt
+++ application/KotlinConcepts/app/src/main/java/com/istudio/app/modules/module_demos/basic_fundamentals/ui/KotlinBasicsDemo.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,8 +20,12 @@ fun KotlinBasicsDemo(navController: NavHostController) {
 
     val viewModel: KotlinBasicsDemoVm = hiltViewModel()
 
+    val scrollState = rememberScrollState()
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .verticalScroll(state = scrollState)
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
