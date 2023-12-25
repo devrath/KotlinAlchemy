7c29ccf259e0f0a199b71cf513414c5d702783c1
Devrath <devrath.dev595@gmail.com>
2023-12-25 13:26:58 +0530

    added code

--- application/KotlinConcepts/app/src/main/java/com/istudio/app/modules/module_demos/flows/modules/terminal_operators/TerminalOperators.kt
+++ application/KotlinConcepts/app/src/main/java/com/istudio/app/modules/module_demos/flows/modules/terminal_operators/TerminalOperators.kt
@@ -1,13 +1,46 @@
 package com.istudio.app.modules.module_demos.flows.modules.terminal_operators
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.istudio.app.modules.module_selection.ModuleDemo
+import com.istudio.app.ui.composables.AppButton
+import com.istudio.app.ui.composables.AppText
 
 @Composable
 fun TerminalOperators(navController: NavHostController){
 
     val viewModel: TerminalOperatorsVm = hiltViewModel()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AppText(text = "Terminal Operators")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AppButton(text = "First", onClick = { viewModel.demoFirst() })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AppButton(text = "Last", onClick = { viewModel.demoLast() })
+
+
+
+    }
 
 
 }
--- application/KotlinConcepts/app/src/main/java/com/istudio/app/modules/module_demos/flows/modules/terminal_operators/TerminalOperatorsVm.kt
+++ application/KotlinConcepts/app/src/main/java/com/istudio/app/modules/module_demos/flows/modules/terminal_operators/TerminalOperatorsVm.kt
@@ -2,15 +2,51 @@ package com.istudio.app.modules.module_demos.flows.modules.terminal_operators
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.lastOrNull
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TerminalOperatorsVm @Inject constructor(
     @ApplicationContext private val context: Context,
 ) : ViewModel() {
+    companion object {
+        const val emissionDelay : Long = 100
+    }
 
+    private val terminalOperatorDemo = flow <Int>{
+        delay(emissionDelay)
+        println("Emitting first value")
+        emit(1)
+        delay(emissionDelay)
+        println("Emitting second value")
+        emit(2)
+    }
 
+    /** *********************** DEMO's *********************** **/
+    /**
+     * Terminal Operator: First
+     */
+    fun demoFirst() {
+        viewModelScope.launch {
+            val result = terminalOperatorDemo.firstOrNull()
+            println("Result:-> $result")
+        }
+    }
 
+    fun demoLast() {
+        viewModelScope.launch {
+            val result = terminalOperatorDemo.lastOrNull()
+            println("Result:-> $result")
+        }
+    }
+
+    /** *********************** DEMO's *********************** **/
 }
