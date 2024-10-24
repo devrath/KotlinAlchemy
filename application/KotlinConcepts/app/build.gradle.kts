221da43124dd5d6067b6f83ebe025d625636e3e7
Devrath <devrath.dev595@gmail.com>
2024-10-24 18:47:48 +0530

    added new upgrade

--- application/KotlinConcepts/app/build.gradle.kts
+++ application/KotlinConcepts/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
--- application/KotlinConcepts/build.gradle.kts
+++ application/KotlinConcepts/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.24" apply false
 }
